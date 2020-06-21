e78628413f1b988fc27af808bb12d21c54ef14be
SpaceDonut <martinkol3v@gmail.com>
2020-06-21 11:27:42 +0900

    Added mongodb and basic file strut

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
 let campgrounds = [
   {name: "Tuna creekest", image:"https://static.wixstatic.com/media/3332be_854f115fac294b37926e692abf3fa86f~mv2.jpg/v1/fill/w_560,h_260,al_c,q_80,usm_0.66_1.00_0.01/DSC01079-A.webp"},
   {name: "Bumblebee hill", image:"https://inteng-storage.s3.amazonaws.com/img/iea/MRw4y5ABO1/sizes/camping-tech-trends_resize_md.jpg"},
+  {name: "Mountain Goat God's rest", image:"https://www.shumarinai.jp/wp-content/uploads/2017/04/cseijikazui-lakeshumarinaicamping-001.jpg"},
+  {name: "Tuna creekest", image:"https://static.wixstatic.com/media/3332be_854f115fac294b37926e692abf3fa86f~mv2.jpg/v1/fill/w_560,h_260,al_c,q_80,usm_0.66_1.00_0.01/DSC01079-A.webp"},
+  {name: "Bumblebee hill", image:"https://inteng-storage.s3.amazonaws.com/img/iea/MRw4y5ABO1/sizes/camping-tech-trends_resize_md.jpg"},
+  {name: "Mountain Goat God's rest", image:"https://www.shumarinai.jp/wp-content/uploads/2017/04/cseijikazui-lakeshumarinaicamping-001.jpg"},
+  {name: "Tuna creekest", image:"https://static.wixstatic.com/media/3332be_854f115fac294b37926e692abf3fa86f~mv2.jpg/v1/fill/w_560,h_260,al_c,q_80,usm_0.66_1.00_0.01/DSC01079-A.webp"},
+  {name: "Bumblebee hill", image:"https://inteng-storage.s3.amazonaws.com/img/iea/MRw4y5ABO1/sizes/camping-tech-trends_resize_md.jpg"},
   {name: "Mountain Goat God's rest", image:"https://www.shumarinai.jp/wp-content/uploads/2017/04/cseijikazui-lakeshumarinaicamping-001.jpg"}
 ];
 
